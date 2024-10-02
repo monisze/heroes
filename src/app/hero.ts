a5394096f9f24650a9f3b73154a535ad4263f7c1
Monise Magalhães <magalhaesmonise@gmail.com>
2024-10-02 19:34:54 -0400

    heroes

---
+++ src/app/hero.ts
@@ -0,0 +1,5 @@
+export interface heroes {
+    id: number;
+    name: string;
+ 
+}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { heroes } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+heroes = HEROES
+
+}
